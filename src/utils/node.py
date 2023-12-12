0faa7a58679f00673731e9bfee5963bf11e8ad63
Thaddeus  Thomas <tthomas013@regis.edu>
2023-12-11 20:34:42 -0700

    update node and stack

--- src/utils/node.py
+++ src/utils/node.py
@@ -1,61 +1,53 @@
-"""mode.py
-_summary_
+"""node.py
+This module contains the definition of the Node class.
 
-_extended_summary_
-
-Returns:
-    _type_: _description_
+The Node class is a basic building block for various data structures like linked lists. Each Node instance
+holds a value and a reference to another Node, enabling the construction of linked or chain-like structures.
 """
-
 
 class Node:
     """
-     _summary_
+    A Node is a fundamental part of data structures like linked lists. 
+    It contains a value and a reference to another node, forming a chain.
 
-    _extended_summary_
+    Attributes:
+        value: The data value stored in the node.
+        link_node: A reference to the next node in the chain, if any.
     """
     def __init__(self, value, link_node=None):
         """
-        __init__ _summary_
-
-        _extended_summary_
+        Initialize a new Node instance.
 
         Args:
-            value (_type_): _description_
-            link_node (_type_, optional): _description_. Defaults to None.
+            value: The value to be stored in the node. The type of value depends on the usage context of the node.
+            link_node (Node, optional): A reference to another Node object. This is used to link nodes together in structures like linked lists. Defaults to None.
         """
         self.value = value
         self.link_node = link_node
 
     def set_next_node(self, link_node):
         """
-        set_next_node _summary_
-
-        _extended_summary_
+        Set the reference to the next node in the chain.
 
         Args:
-            link_node (_type_): _description_
+            link_node (Node): A Node object that will be the next node in the chain.
         """
         self.link_node = link_node
 
     def get_next_node(self):
         """
-        get_next_node _summary_
-
-        _extended_summary_
+        Retrieve the next node in the chain.
 
         Returns:
-            _type_: _description_
+            Node: The next node in the chain. Returns None if there is no linked node.
         """
         return self.link_node
 
     def get_value(self):
         """
-        get_value _summary_
-
-        _extended_summary_
+        Get the value stored in the node.
 
         Returns:
-            _type_: _description_
+            The value stored in the node. The type of the value depends on how the node is used.
         """
         return self.value
--- src/utils/stack.py
+++ src/utils/stack.py
@@ -1,15 +1,54 @@
-# stack.py
+""" stack.py
+
+This module defines the Stack class, a basic implementation of a stack data 
+structure using linked nodes.
+
+Stacks are a type of data structure that follow a 
+Last In, First Out (LIFO) principle, where the last item added is the first to 
+be removed.
+
+This Stack class includes methods to push, pop, peek, and check the size and capacity of the stack.
+
+Returns:
+    _type_: _description_
+"""
 
 from utils.node import Node
 
 class Stack:
+    """
+    A Stack is a collection of elements with a LIFO (Last In, First Out) principle. 
+    This implementation uses linked nodes to store data.
+
+    Attributes:
+        size (int): The number of elements in the stack.
+        top_item (Node): The top item of the stack.
+        limit (int): The maximum capacity of the stack.
+        name (str): The name of the stack.
+    """
     def __init__(self, name):
+        """
+        Initialize a new Stack instance.
+
+        Args:
+            name (str): The name of the stack.
+        """
         self.size = 0
         self.top_item = None
         self.limit = 1000
         self.name = name
 
     def push(self, value):
+        """
+        Add a new item to the top of the stack.
+
+        If the stack is full, it will output a message indicating no more items 
+        can be added.
+
+        Args:
+            value: The value to be added to the stack. The type depends on the 
+            stack's usage.
+        """
         if self.has_space():
             item = Node(value)
             item.set_next_node(self.top_item)
@@ -19,6 +58,16 @@ class Stack:
             print("No more room!")
 
     def pop(self):
+        """
+        Remove and return the top item from the stack.
+
+        If the stack is empty, it outputs a message indicating that there are 
+        no items to pop.
+
+        Returns:
+            The value of the top item. The type of the value depends on the 
+            stack's usage.
+        """
         if self.size > 0:
             item_to_remove = self.top_item
             self.top_item = item_to_remove.get_next_node()
@@ -27,23 +76,61 @@ class Stack:
         print("This stack is totally empty.")
 
     def peek(self):
+        """
+        Return the value of the top item without removing it from the stack.
+
+        Returns:
+            The value of the top item. Returns None if the stack is empty. The 
+            type of the value depends on the stack's usage.
+        """
         if self.size > 0:
             return self.top_item.get_value()
         print("Nothing to see here!")
 
     def has_space(self):
+        """
+        Check if the stack has space to add more items.
+
+        Returns:
+            bool: True if the stack has space, False otherwise.
+        """
         return self.limit > self.size
 
     def is_empty(self):
+        """
+        Check if the stack is empty.
+
+        Returns:
+            bool: True if the stack is empty, False otherwise.
+        """
         return self.size == 0
 
     def get_size(self):
+        """
+        Get the current size of the stack.
+
+        Returns:
+            int: The number of items in the stack.
+        """
         return self.size
 
     def get_name(self):
+        """
+        Get the name of the stack.
+
+        Returns:
+            str: The name of the stack.
+        """
         return self.name
 
     def print_items(self):
+        """
+        Print all items in the stack.
+
+        This method traverses the stack from top to bottom and prints each 
+        item's value. The stack's current state is displayed in the order items 
+        would be popped.
+        """
         pointer = self.top_item
         print_list = []
         while pointer:
